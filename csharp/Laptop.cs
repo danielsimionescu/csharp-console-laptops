6b83a22deb042c8be2d1f3becbdfdad52ce2e614
Daniel <danielsimionescu1996@gmail.com>
2018-06-09 21:29:55 +0300

    Add project files.

---
+++ csharp/Laptop.cs
@@ -0,0 +1,14 @@
+﻿namespace csharp
+{
+    internal class Laptop
+    {
+        public string Manufacturer { get; set; }
+        public string Model { get; set; }
+        public string Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Manufacturer} {Model}";
+        }
+    }
+}
---
+++ csharp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var laptops = new Dictionary<int, Laptop>
+            {
+                [1] = new Laptop
+                {
+                    Manufacturer = "HP",
+                    Model = "XPS 15",
+                    Price = "2000"
+                },
+                [2] = new Laptop
+                {
+                    Manufacturer = "Razor",
+                    Model = "Blade 15",
+                    Price = "2500"
+                },
+                [3] = new Laptop
+                {
+                    Manufacturer = "Apple",
+                    Model = "Macbook Pro",
+                    Price = "3000"
+                }
+            };
+
+            foreach (var laptop in laptops)
+            {
+                Console.WriteLine($"{laptop.Key}. {laptop.Value}");
+            }
+
+            do
+            {
+                Console.Write("\nInsert the No to get the price... ");
+                var input = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(input))
+                {
+                    if (int.TryParse(input, out int id))
+                    {
+                        if (laptops.ContainsKey(id))
+                        {
+                            var laptop = laptops[id];
+                            Console.WriteLine($"You chose {laptop} which is {laptop.Price}$");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Key {input} is invalid.");
+                        }
+                    }
+                }
+            } while (true);
+        }
+    }
+}
